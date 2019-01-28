51cfba38fd3acf596a920ad54d841d3ac066ca3b
Keenan <Keenanking555@gmail.com>
2019-01-28 12:40:17 -0700

    Node created! Wow!

---
+++ StructureProject/Controller/Runner.cpp
@@ -0,0 +1,16 @@
+//
+//  Runner.cpp
+//  StructureProject
+//
+//  Created by Davis, Keenan on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+int main()
+{
+    Controller * app = new Controller;
+    
+    app->start();
+}
